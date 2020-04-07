690d63dde0dffbf03debfbff1c10c18525982b8a
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-04-07 20:19:04 +0100

    Constructors created for all the classes

--- src/main/java/com/codeclan/example/FilesManager/models/Folder.java
+++ src/main/java/com/codeclan/example/FilesManager/models/Folder.java
@@ -1,4 +1,18 @@
 package com.codeclan.example.FilesManager.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Folder {
+
+    private String title;
+    private List<File>files;
+    private User user;
+
+
+    public Folder(String title, User user) {
+        this.title = title;
+        this.user = user;
+        this.files=new ArrayList<>();
+    }
 }
--- src/main/java/com/codeclan/example/FilesManager/models/User.java
+++ src/main/java/com/codeclan/example/FilesManager/models/User.java
@@ -1,4 +1,31 @@
 package com.codeclan.example.FilesManager.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+
+    private String name;
+    private List<Folder>folders;
+
+    public User(String name) {
+        this.name = name;
+        this.folders = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public void setFolders(List<Folder> folders) {
+        this.folders = folders;
+    }
 }
